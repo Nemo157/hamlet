957a3d82a487a30c36fccebee104371288a7a45a
Wim Looman <wim@nemo157.com>
2016-03-28 13:23:33 +0200

    Make events responsible for writing themselves

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod macros;
 mod event;
 mod attribute;
 mod write;
+mod fmt;
 
 pub use event::Event;
 pub use attribute::Attribute;
--- src/write.rs
+++ src/write.rs
@@ -12,9 +12,9 @@ impl From<fmt::Error> for Error {
 
 pub trait Write {
     fn write_event(&mut self, event: Event) -> Result<(), Error>;
-    fn write_events<'a, I: IntoIterator<Item = Event<'a>>>(&mut self,
-                                                           events: I)
-                                                           -> Result<(), Error> {
+    fn write_events<'a, I>(&mut self, events: I) -> Result<(), Error>
+        where I: IntoIterator<Item = Event<'a>>
+    {
         for event in events {
             try!(self.write_event(event));
         }
@@ -24,34 +24,7 @@ pub trait Write {
 
 impl<T: fmt::Write> Write for T {
     fn write_event(&mut self, event: Event) -> Result<(), Error> {
-        match event {
-            Event::StartTag { name, attrs } => {
-                try!(write!(self, "<{}", name));
-                for attr in attrs.into_iter() {
-                    // TODO: Escaping
-                    try!(write!(self, " {}=\"{}\"", attr.name, attr.value));
-                }
-                try!(write!(self, ">"));
-            }
-            Event::ClosedTag { name, attrs } => {
-                try!(write!(self, "<{}", name));
-                for attr in attrs.into_iter() {
-                    // TODO: Escaping
-                    try!(write!(self, " {}=\"{}\"", attr.name, attr.value));
-                }
-                try!(write!(self, " />"));
-            }
-            Event::EndTag { name } => {
-                try!(write!(self, "</{}>", name));
-            }
-            Event::Text(text) => {
-                // TODO: Escaping
-                try!(write!(self, "{}", text));
-            }
-            Event::RawHtml(html) => {
-                try!(write!(self, "{}", html));
-            }
-        }
+        try!(write!(self, "{}", event));
         Ok(())
     }
 }
