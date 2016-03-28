cb5e0226896858e9e42c8802ff729ea084d42da4
Wim Looman <wim@nemo157.com>
2016-03-28 14:11:43 +0200

    Reformat macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,73 +1,73 @@
 #[macro_export]
 macro_rules! start_tag {
-  ($name:expr) => {
-    ($crate::Event::StartTag {
-      name: ::std::borrow::Cow::from($name),
-      attrs: $crate::Attribute::none(),
-    })
-  };
-  ($name:expr, $attrs:expr) => {
-    ($crate::Event::StartTag {
-      name: ::std::borrow::Cow::from($name),
-      attrs: $attrs,
-    })
-  };
+    ($name:expr) => {
+        ($crate::Event::StartTag {
+            name: ::std::borrow::Cow::from($name),
+            attrs: $crate::Attribute::none(),
+        })
+    };
+    ($name:expr, $attrs:expr) => {
+        ($crate::Event::StartTag {
+            name: ::std::borrow::Cow::from($name),
+            attrs: $attrs,
+        })
+    };
 }
 
 #[macro_export]
 macro_rules! closed_tag {
-  ($name:expr) => {
-    ($crate::Event::ClosedTag {
-      name: ::std::borrow::Cow::from($name),
-      attrs: $crate::Attribute::none(),
-    })
-  };
-  ($name:expr, $attrs:expr) => {
-    ($crate::Event::ClosedTag {
-      name: ::std::borrow::Cow::from($name),
-      attrs: $attrs,
-    })
-  };
+    ($name:expr) => {
+        ($crate::Event::ClosedTag {
+            name: ::std::borrow::Cow::from($name),
+            attrs: $crate::Attribute::none(),
+        })
+    };
+    ($name:expr, $attrs:expr) => {
+        ($crate::Event::ClosedTag {
+            name: ::std::borrow::Cow::from($name),
+            attrs: $attrs,
+        })
+    };
 }
 
 #[macro_export]
 macro_rules! end_tag {
-  ($name:expr) => {
-    ($crate::Event::EndTag {
-      name: ::std::borrow::Cow::from($name),
-    })
-  };
+    ($name:expr) => {
+        ($crate::Event::EndTag {
+            name: ::std::borrow::Cow::from($name),
+        })
+    };
 }
 
 #[macro_export]
 macro_rules! text {
-  ($text:expr) => {
-    ($crate::Event::Text(::std::borrow::Cow::from($text)))
-  };
+    ($text:expr) => {
+        ($crate::Event::Text(::std::borrow::Cow::from($text)))
+    };
 }
 
 #[macro_export]
 macro_rules! raw_html {
-  ($html:expr) => {
-    ($crate::Event::RawHtml(::std::borrow::Cow::from($html)))
-  };
+    ($html:expr) => {
+        ($crate::Event::RawHtml(::std::borrow::Cow::from($html)))
+    };
 }
 
 #[macro_export]
 macro_rules! attr {
-  ($name:expr => $value:expr) => {
-    ($crate::Attribute {
-      name: ::std::borrow::Cow::from($name),
-      value: ::std::borrow::Cow::from($value),
-    })
-  }
+    ($name:expr => $value:expr) => {
+        ($crate::Attribute {
+            name: ::std::borrow::Cow::from($name),
+            value: ::std::borrow::Cow::from($value),
+        })
+    }
 }
 
 #[macro_export]
 macro_rules! attrs {
-  ($($name:expr => $value:expr),+) => {
-    (::std::borrow::Cow::Owned(vec![
-      $(attr!($name => $value)),+
-    ]))
-  }
+    ($($name:expr => $value:expr),+) => {
+        (::std::borrow::Cow::Owned(vec![
+            $(attr!($name => $value)),+
+        ]))
+    }
 }
