3406ac4321cade1f6560d9f9840ce3e3e0e653d2
Wim Looman <wim@nemo157.com>
2016-03-28 09:46:58 +0200

    Add write trait and impl for fmt::Write

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,8 @@
 pub mod macros;
 mod event;
 mod attribute;
+mod write;
+
 pub use event::Event;
 pub use attribute::Attribute;
+pub use write::Write;
---
+++ src/write.rs
@@ -0,0 +1,55 @@
+use std::fmt;
+use Event;
+
+#[derive(Debug)]
+pub struct Error;
+
+impl From<fmt::Error> for Error {
+  fn from(_: fmt::Error) -> Error {
+    Error
+  }
+}
+
+pub trait Write {
+  fn write_event(&mut self, event: Event) -> Result<(), Error>;
+  fn write_events<'a, I: IntoIterator<Item=Event<'a>>>(&mut self, events: I) -> Result<(), Error> {
+    for event in events {
+      try!(self.write_event(event));
+    }
+    Ok(())
+  }
+}
+
+impl<T: fmt::Write> Write for T {
+  fn write_event(&mut self, event: Event) -> Result<(), Error> {
+    match event {
+      Event::StartTag { name, attrs } => {
+        try!(write!(self, "<{}", name));
+        for attr in attrs.into_iter() {
+          // TODO: Escaping
+          try!(write!(self, " {}=\"{}\"", attr.name, attr.value));
+        }
+        try!(write!(self, ">"));
+      },
+      Event::ClosedTag { name, attrs } => {
+        try!(write!(self, "<{}", name));
+        for attr in attrs.into_iter() {
+          // TODO: Escaping
+          try!(write!(self, " {}=\"{}\"", attr.name, attr.value));
+        }
+        try!(write!(self, " />"));
+      },
+      Event::EndTag { name } => {
+        try!(write!(self, "</{}>", name));
+      },
+      Event::Text(text) => {
+        // TODO: Escaping
+        try!(write!(self, "{}", text));
+      },
+      Event::RawHtml(html) => {
+        try!(write!(self, "{}", html));
+      },
+    }
+    Ok(())
+  }
+}
