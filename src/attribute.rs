691d8ef9d830825207462794e269c88dcfc2570c
Wim Looman <wim@nemo157.com>
2016-03-28 12:52:32 +0200

    rustfmt

--- src/attribute.rs
+++ src/attribute.rs
@@ -4,12 +4,12 @@ static EMPTY_ATTRS: &'static [Attribute<'static>] = &[];
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Attribute<'a> {
-  pub name: Cow<'a, str>,
-  pub value: Cow<'a, str>,
+    pub name: Cow<'a, str>,
+    pub value: Cow<'a, str>,
 }
 
 impl<'a> Attribute<'a> {
-  pub fn none() -> Cow<'a, [Attribute<'a>]> {
-    Cow::Borrowed(EMPTY_ATTRS)
-  }
+    pub fn none() -> Cow<'a, [Attribute<'a>]> {
+        Cow::Borrowed(EMPTY_ATTRS)
+    }
 }
--- src/event.rs
+++ src/event.rs
@@ -4,17 +4,17 @@ use Attribute;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Event<'a> {
-  StartTag {
-    name: Cow<'a, str>,
-    attrs: Cow<'a, [Attribute<'a>]>,
-  },
-  EndTag {
-    name: Cow<'a, str>,
-  },
-  ClosedTag {
-    name: Cow<'a, str>,
-    attrs: Cow<'a, [Attribute<'a>]>,
-  },
-  Text(Cow<'a, str>),
-  RawHtml(Cow<'a, str>),
+    StartTag {
+        name: Cow<'a, str>,
+        attrs: Cow<'a, [Attribute<'a>]>,
+    },
+    EndTag {
+        name: Cow<'a, str>,
+    },
+    ClosedTag {
+        name: Cow<'a, str>,
+        attrs: Cow<'a, [Attribute<'a>]>,
+    },
+    Text(Cow<'a, str>),
+    RawHtml(Cow<'a, str>),
 }
