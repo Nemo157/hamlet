06e9ed8f1ffcbb645a0e45bac624bcd3458f86df
critiqjo <john.ch.fr@gmail.com>
2016-03-29 23:08:55 +0530

    HtmlStreamer should stream!

--- src/stream.rs
+++ src/stream.rs
@@ -1,64 +1,36 @@
 use std::io;
-use std::io::Write;
 use std::marker::PhantomData;
 use Event;
 
-pub struct HtmlStreamer<'a, I>
-    where I: Iterator<Item = Event<'a>> + 'a
+pub struct HtmlStreamer<'a, I, J>
+    where I: IntoIterator<IntoIter = J, Item = Event<'a>> + 'a,
+          J: Iterator<Item = Event<'a>>
 {
     events: I,
-    buffer: Vec<u8>,
     ev_life: PhantomData<Event<'a>>,
 }
 
-impl<'a, I> HtmlStreamer<'a, I>
-    where I: Iterator<Item = Event<'a>>
+impl<'a, I, J> HtmlStreamer<'a, I, J>
+    where I: IntoIterator<IntoIter = J, Item = Event<'a>> + 'a,
+          J: Iterator<Item = Event<'a>>
 {
-    pub fn new<II>(events: II) -> HtmlStreamer<'a, I>
-        where II: IntoIterator<IntoIter = I, Item = Event<'a>>
-    {
+    pub fn new(events: I) -> HtmlStreamer<'a, I, J> {
         HtmlStreamer {
-            events: events.into_iter(),
-            buffer: Vec::new(),
+            events: events,
             ev_life: PhantomData,
         }
     }
-}
 
-impl<'a, I> io::Read for HtmlStreamer<'a, I>
-    where I: Iterator<Item = Event<'a>>
-{
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        while self.buffer.len() == 0 {
-            if let Some(event) = self.events.next() {
-                // Possible to do better?
-                try!(write!(&mut self.buffer, "{}", event));
-            } else {
-                return Ok(0);
-            }
+    pub fn stream(self, w: &mut io::Write) -> io::Result<usize> {
+        for ev in self.events.into_iter() {
+            try!(write!(w, "{}", ev));
         }
-        let curlen = self.buffer.len();
-        let len = ::std::cmp::min(curlen, buf.len());
-        buf[..len].clone_from_slice(&self.buffer[..len]);
-        for i in len..curlen {
-            self.buffer[i - len] = self.buffer[i];
-        }
-        self.buffer.truncate(curlen - len);
-        Ok(len)
-    }
-
-    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> io::Result<usize> {
-        let start = buf.len();
-        while let Some(event) = self.events.next() {
-            try!(write!(buf, "{}", event));
-        }
-        Ok(buf.len() - start)
+        return Ok(0);
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::io::Read;
     use HtmlStreamer;
 
     #[test]
@@ -74,10 +46,12 @@ mod tests {
             end_tag!("h1"),
         ];
 
-        let mut result = String::new();
-        HtmlStreamer::new(events).read_to_string(&mut result).unwrap();
+        let mut result = Vec::new();
+        HtmlStreamer::new(events).stream(&mut result).unwrap();
+        let res_str = String::from_utf8(result).unwrap();
 
-        assert_eq!(result, "<h1 id=\"hello\" class=\"fun\">Hello, \
-                            <small>world</small><img src=\"foo-link\" /></h1>");
+        assert_eq!(res_str.as_str(),
+                   "<h1 id=\"hello\" class=\"fun\">Hello, \
+                    <small>world</small><img src=\"foo-link\" /></h1>");
     }
 }
