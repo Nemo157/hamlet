c63c0d05ba94064e241c950475c9bdb83290d6e9
Wim Looman <wim@nemo157.com>
2016-03-31 21:46:11 +0200

    Simplify identifier_to_tag_name

--- src/util.rs
+++ src/util.rs
@@ -1,29 +1,22 @@
+use std::ascii::AsciiExt;
 use std::borrow::Cow;
 
 pub fn identifier_to_tag_name<'a, T>(s: T) -> Cow<'a, str>
     where T: Into<Cow<'a, str>>
 {
-    let mut owned: Option<String> = None;
     let s = s.into();
-    {
-        let bs: &str = s.as_ref();
-        for (i, c) in bs.chars().enumerate() {
-            match c {
-                c @ 'A' ... 'Z' => {
-                    let mut os = owned.unwrap_or({
-                        let mut os = String::with_capacity(bs.len() + 4);
-                        os.extend((&bs[..i]).chars());
-                        os
-                    });
-                    os.push('-');
-                    os.push(c.to_lowercase().next().unwrap());
-                    owned = Some(os);
-                }
-                _ => if let Some(ref mut os) = owned {
-                    os.push(c);
-                }
+    if s.contains(|c: char| c.is_ascii() && c.is_uppercase()) {
+        let mut result = String::with_capacity(s.len() + 4);
+        for c in s.chars() {
+            if c.is_ascii() && c.is_uppercase() {
+                result.push('-');
+                result.push(c.to_ascii_lowercase());
+            } else {
+                result.push(c);
             }
         }
+        Cow::Owned(result)
+    } else {
+        s
     }
-    owned.map(|os| os.into()).unwrap_or(s)
 }
