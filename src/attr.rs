003b826d77806620cc68220cd4d5c1fecd39d829
Wim Looman <wim@nemo157.com>
2016-04-01 19:38:53 +0200

    Simplify attr accessors

--- src/attr.rs
+++ src/attr.rs
@@ -32,38 +32,29 @@ impl<'a> fmt::Display for Attribute<'a> {
 pub struct AttributeSet<'a>(pub Cow<'a, [Attribute<'a>]>);
 
 impl<'a> AttributeSet<'a> {
-    fn search_name<S>(&self, name: S) -> Option<usize>
-        where S: AsRef<str>
-    {
-        for (i, attr) in self.0.iter().enumerate() {
-            if attr.name == name.as_ref() {
-                return Some(i);
-            }
-        }
-        None
-    }
-
     pub fn get_attr<S>(&self, name: S) -> Option<&Attribute<'a>>
         where S: AsRef<str>
     {
-        self.search_name(name).map(|index| &self.0.as_ref()[index])
+        self.0.iter().find(|attr| attr.name == name.as_ref())
     }
 
     pub fn get_attr_mut<S>(&mut self, name: S) -> Option<&mut Attribute<'a>>
         where S: AsRef<str>
     {
-        self.search_name(name).map(move |index| &mut self.0.to_mut()[index])
+        self.0.to_mut().iter_mut().find(|attr| attr.name == name.as_ref())
     }
 
     pub fn set_attr<N, V>(&mut self, name: N, value: V)
         where N: Into<Cow<'a, str>>,
               V: Into<Cow<'a, str>>
     {
-        let name = name.into();
-        if let Some(index) = self.search_name(name.as_ref()) {
-            self.0.to_mut()[index].value = value.into();
+        let (name, value) = (name.into(), value.into());
+        let attrs = self.0.to_mut();
+
+        if let Some(pos) = attrs.iter().position(|attr| attr.name == name) {
+            attrs[pos].value = value;
         } else {
-            self.0.to_mut().push(Attribute::new(name, value));
+            attrs.push(Attribute::new(name, value));
         }
     }
 }
