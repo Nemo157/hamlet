b83593e75846b1305752b5d7bacaa39154875951
Wim Looman <wim@nemo157.com>
2016-04-09 13:30:02 +0200

    Add docs on public modules

--- src/lib.rs
+++ src/lib.rs
@@ -30,11 +30,14 @@
 
 #![warn(missing_docs)]
 
+/// Currently contains just a semi-private utility function to support the
+/// [`attrs!`](./macro.attrs!.html) macro.
 pub mod util;
 
 #[macro_use]
 mod macros;
 
+/// Contains structs for defining attributes on elements.
 pub mod attr;
 mod escape;
 mod token;
