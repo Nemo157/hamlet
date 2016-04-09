179cd9ff0267e3b97729f5e18adea33c7a07cd84
Wim Looman <wim@nemo157.com>
2016-04-09 15:40:21 +0200

    Remove verification of tag

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,4 @@ then
   echo "Tag $TAG doesn't appear to be the version in Cargo.toml"
 fi
 
-gpg --keyserver pgp.mit.edu --recv-keys E583F750
-gpg --list-keys
-git tag -v "$TAG"
 cargo publish --verbose --token "$TOKEN"
