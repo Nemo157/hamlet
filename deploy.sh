1804b1a646f9367cdf6f7ff49d22772e0bdd2a10
Wim Looman <wim@nemo157.com>
2016-04-09 15:40:39 +0200

    Add autodeployment of docs to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,23 @@ then
 fi
 
 cargo publish --verbose --token "$TOKEN"
+
+# Setup this repo to publish the docs
+git fetch origin gh-pages
+git checkout -b gh-pages FETCH_HEAD
+
+# Move the built docs into versioned folder
+mv target/doc docs/$TAG
+
+# Update the index to point to the versioned docs
+sed -i'' -e '/<!-- TRAVIS: PUT THE DOCS HERE-->/a\
+<li><a href="docs/'"$TAG"'/hamlet/">'"$TAG"'</a></li>
+' index.html
+
+# Add the changes
+git add docs/$TAG
+git add index.html
+
+# Commit and push
+git commit -m "Add API docs for $TAG"
+git push origin gh-pages:gh-pages
